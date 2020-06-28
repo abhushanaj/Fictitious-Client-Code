6a424dd1e923e466c7c6a2d50f28ebf3b0193d19
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-06-28 22:58:50 +0530

    Logic for the sharing toggle on small and bigger screens completed

--- Article-Preview-Component/scripts/index.js
+++ Article-Preview-Component/scripts/index.js
@@ -1,10 +1,32 @@
 const shareButton = document.getElementById("shareButton");
 const sharingMenu = document.getElementById("sharingMenu");
+const smallSharingMenu = document.getElementById("smallSharingMenu");
+const shareButtonSmall = document.getElementById("shareButtonSmall");
+
 shareButton.addEventListener('click', () => {
-    if (sharingMenu.classList.contains("hide")) {
-        sharingMenu.classList.remove('hide');
+    console.log("click");
+    const screenWidthSize = screen.width;
+    // handle larger screens (greater than 450px)
+    if (screenWidthSize > 450) {
+        if (sharingMenu.classList.contains("hide")) {
+            sharingMenu.classList.remove('hide');
+        }
+        else {
+            sharingMenu.classList.add('hide');
+        }
     }
     else {
-        sharingMenu.classList.add('hide');
+        shareButton.classList.add("remove");
+        smallSharingMenu.classList.remove("hide");
     }
+    
 });
+
+shareButtonSmall.addEventListener('click', () => {
+    console.log("Click from small screen");
+    if (!smallSharingMenu.classList.contains("hide")) {
+        sharingMenu.classList.remove('hide');
+        smallSharingMenu.classList.add("hide");
+        shareButton.classList.remove("remove");
+    }
+})
