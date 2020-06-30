94f28f7011209fae2e6ec3958cbcc64f79495965
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-06-30 23:26:38 +0530

    Completed the small screen navbar and responsiveness in small screen comleted

--- Project-Tracking-Intro-Component/scripts/index.js
+++ Project-Tracking-Intro-Component/scripts/index.js
@@ -1,5 +1,16 @@
 const hamburger = document.getElementById('hamburger');
+const smallNavigation = document.getElementById('navbar__small');
+const CLOSE_BURGER = './images/icon-close.svg';
+const OPEN_BURGER = './images/icon-hamburger.svg';
 
 hamburger.addEventListener('click', () => {
-    console.log('Click');
+    const isHidden = smallNavigation.classList.contains('hide');
+    if (isHidden) {
+        smallNavigation.classList.remove('hide');
+        hamburger.src = CLOSE_BURGER;
+    }
+    else {
+        smallNavigation.classList.add('hide');
+        hamburger.src = OPEN_BURGER;
+    }
 })
